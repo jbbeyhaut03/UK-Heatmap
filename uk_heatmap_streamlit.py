9116b914482e11b2d093e786f15cdefb21af9cc7
jbbeyhaut03 <jbbeyhaut@gmail.com>
2025-05-14 17:06:14 +0200

    ratio calculation

--- uk_heatmap_streamlit.py
+++ uk_heatmap_streamlit.py
@@ -6,18 +6,12 @@ from streamlit_folium import folium_static
 import os
 
 # Load and aggregate data
-geocoded_file_path = 'geocoded_zip_codes.csv'
-if os.path.exists(geocoded_file_path):
-    df = pd.read_csv(geocoded_file_path)
-    aggregated_data = df.groupby(['Zip Code', 'Status', 'Latitude', 'Longitude']).size().reset_index(name='Count')
-else:
-    aggregated_data = pd.DataFrame({
-        'Zip Code': ['XYZ', 'ABC'],
-        'Status': ['No Show', 'Completed'],
-        'Count': [10, 15],
-        'Latitude': [51.5, 53.4],
-        'Longitude': [-0.1, -2.3]
-    })
+geocoded_file_path = 'dentist_data_map.csv'
+df = pd.read_csv(geocoded_file_path)
+receiving_cases = df.groupby(['postcode', 'latitude', 'longitude'])['receiving_cases'] * 30 
+availability = df['weekly_availability_hours'] * 60 
+ratio = availability / receiving_cases
+
 
 # Streamlit app title
 st.title("UK Postal Code Density Map with Status Filter")
@@ -29,8 +23,6 @@ status = st.selectbox("Select Status", options=['All'] + list(aggregated_data['S
 filtered_data = aggregated_data if status == 'All' else aggregated_data[aggregated_data['Status'] == status]
 
 # Create Folium map with heatmap
-
-
 m = folium.Map(location=[55.3781, -3.4360], zoom_start=6)
 heat_data = [[row['Latitude'], row['Longitude'], row['Count']] for _, row in filtered_data.iterrows()]
 HeatMap(heat_data, radius=15, max_zoom=13).add_to(m)
